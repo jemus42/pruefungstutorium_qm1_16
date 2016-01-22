abbc31e25794b939209f521c7505e9fc08dfa29b
jemus42 <jemus42@quantenbrot.de>
2016-01-22 22:19:39 +0100

    Add buildscript fancyness

--- build.R
+++ build.R
@@ -1,9 +1,15 @@
 #! /usr/bin/env Rscript
 
-rmarkdown::render(input = "Presentation.Rmd",
+rmarkdown::render(input         = "Presentation.Rmd",
                   output_format = "ioslides_presentation",
-                  output_file = "Presentation.html")
+                  output_file   = "Presentation.html")
 
-rmarkdown::render(input = "Presentation.Rmd",
+rmarkdown::render(input         = "Presentation.Rmd",
                   output_format = "html_document",
-                  output_file = "Presentation_web.html")
+                  output_file   = "Presentation_web.html")
+
+## Push to my filedump
+
+if (system("whoami", intern = TRUE) == "Lukas" & Sys.Date() > "2016-02-08") {
+  file.copy("*html", "~/repos/R/Tutorium/qm-sync/QM1/", recursive = TRUE)
+}
